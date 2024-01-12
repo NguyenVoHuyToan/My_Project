5717fec2f6adf42d12796bcdabf92c7c2ae78409
ybinh07 <yenbinh18082307@gmail.com>
2024-01-12 20:10:01 +0700

    login

--- sever/routes/userRoute.js
+++ sever/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { getOAuth } from "../Service/userService.js";
-import { loginValidator } from "../Service/validator.js";
+import { loginValidator, validateRegister } from "../Service/validator.js";
 import { accessTokenController } from "../Controller/accessToken-controller.js";
 export const userRoute=express.Router();
 userRoute.get("/oath",getOAuth);
 userRoute.post("/login", loginValidator, accessTokenController);
+userRoute.post("/register",validateRegister, registerMiddleware, registerController);
