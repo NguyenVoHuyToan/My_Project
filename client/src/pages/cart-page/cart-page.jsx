42d0f431bef493ecdbde48763e5694547444899b
dophambichtram <bichtramdopham@gmail.com>
2024-01-16 22:25:37 +0700

    up

--- client/src/pages/cart-page/cart-page.jsx
+++ client/src/pages/cart-page/cart-page.jsx
@@ -20,8 +20,7 @@ const Cartpage = () => {
         const authToken = localStorage.getItem("token");
 
         console.log(authToken == "null");
-        if (authToken == "null"||authToken == "undefined" || !authToken) {
-          
+        if (authToken == "null" || authToken == "undefined" || !authToken) {
           // throw new Error(
           //   "User not logged in. Please log in to view cart items."
           // );
@@ -45,11 +44,7 @@ const Cartpage = () => {
             : data && Array.isArray(data.items)
             ? data.items
             : [];
-<<<<<<< HEAD
 
-=======
-          
->>>>>>> ce9ad5f59cda4765fbfc768bfafb35517c15ef31
           setUserProducts(cartItems);
           setLoading(false);
         }
@@ -67,24 +62,14 @@ const Cartpage = () => {
   useEffect(() => {
     const fetchAllProducts = async () => {
       try {
-<<<<<<< HEAD
-        const response = await fetch(`http://localhost:3000/product/carts`);
-        const data = await response.json();
-
-        setAllProducts(data);
-=======
         const authToken = localStorage.getItem("token");
-        
-          const data = await axios.post(
-            `http://localhost:3000/product/cartOne`, {
-              accessToken:authToken
-            }
-          );
-          
-          
+
+        const data = await axios.post(`http://localhost:3000/product/cartOne`, {
+          accessToken: authToken,
+        });
+
         console.log(data.data);
         setAllProducts(data.data);
->>>>>>> ce9ad5f59cda4765fbfc768bfafb35517c15ef31
       } catch (error) {
         setError("An error occurred while fetching product details.");
       }
@@ -155,36 +140,22 @@ const Cartpage = () => {
               <p className="body-bld capitalize">price</p>
             </div>
             <div className="order-items flex-col max-wdth gap-xs">
-<<<<<<< HEAD
-              {allProducts.map((product, index) => (
-                <div
-                  key={product.product_id}
-                  className="item flex-row body-sml align-left max-wdth"
-                >
-                  <p className="product-name">
-                    {product.product_des[index].product_name}
-                  </p>
-                  <p className="product-price">
-                    {dongFormatter(product.product_des[index].price * 1000)}
-                  </p>
-                </div>
-              ))}
-=======
-              {allProducts.map((product,index) => {
+              {allProducts.map((product, index) => {
                 console.log(product);
                 return (
                   <div
                     key={product.product_id}
                     className="item flex-row body-sml align-left max-wdth"
                   >
-                    <p className="product-name">{product.product_des[index].product_name}</p>
+                    <p className="product-name">
+                      {product.product_des[index].product_name}
+                    </p>
                     <p className="product-price">
                       {dongFormatter(product.product_des[index].price * 1000)}
                     </p>
                   </div>
-                )
+                );
               })}
->>>>>>> ce9ad5f59cda4765fbfc768bfafb35517c15ef31
             </div>
           </div>
           <div className="hr-divider"></div>
