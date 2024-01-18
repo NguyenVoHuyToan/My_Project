742e08cf533d22036205d87fe9f135aba0d15d00
dophambichtram <bichtramdopham@gmail.com>
2024-01-18 18:03:26 +0700

    up

--- client/src/pages/cart-page/cart-page.jsx
+++ client/src/pages/cart-page/cart-page.jsx
@@ -13,10 +13,10 @@ const Cartpage = () => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [calculateTotal,setCalcualteTotal]=useState(false);
-  const getTotal=()=>{
+  const [calculateTotal, setCalcualteTotal] = useState(false);
+  const getTotal = () => {
     let total = 0;
-  
+
     // for (let index = 0; index < aList; index++) {
     //   const element = userProducts;
     //   console.log(element);
@@ -26,14 +26,14 @@ const Cartpage = () => {
     //   });
     // }
     console.log(userProducts);
-    userProducts.cart.map((item,index)=>{
-      total+=item.quantity* userProducts.product_des[index].price;
-    })
+    userProducts.cart.map((item, index) => {
+      total += item.quantity * userProducts.product_des[index].price;
+    });
 
-    total = total*0.8+total*0.1+ 5;
+    total = total * 0.8 + total * 0.1 + 5;
     console.log(total);
     setTotalPrice(total);
-  }
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -53,16 +53,17 @@ const Cartpage = () => {
               accessToken: authToken,
             }
           );
-            console.log("response",response);
+          console.log("response", response);
           if (!response) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
 
           const data = response;
 
-          const cartItems = data.data.map((item,index)=>{
-            return item
-          })||[];
+          const cartItems =
+            data.data.map((item, index) => {
+              return item;
+            }) || [];
           console.log(cartItems[0]);
           setUserProducts(cartItems[0]);
           setLoading(false);
@@ -74,9 +75,9 @@ const Cartpage = () => {
         setLoading(false);
       }
     };
-    
+
     fetchData();
-   
+
     // if(userProducts.length>0){
     //   getTotal()
     // }
@@ -93,7 +94,7 @@ const Cartpage = () => {
 
   //       console.log(data.data[0]);
   //       setAllProducts(data.data[0]);
-        
+
   //     } catch (error) {
   //       setError("An error occurred while fetching product details.");
   //     }
@@ -102,15 +103,15 @@ const Cartpage = () => {
   //   fetchAllProducts();
   // }, []);
 
-  useEffect(()=>{
-    if(Object.keys(userProducts).length>0){
+  useEffect(() => {
+    if (Object.keys(userProducts).length > 0) {
       getTotal();
     }
-  },[userProducts])
+  }, [userProducts]);
 
   const deleteProduct = (productId) => {
     setUserProducts(
-      userProducts.filter((product) => product.cart.productId !== productId)
+      userProducts.filter((product) => product.productId !== productId)
     );
   };
 
@@ -130,22 +131,21 @@ const Cartpage = () => {
   if (error) {
     return <p>{error}</p>;
   }
- 
+
   return (
     <div className="cart-page flex-row gap-sm align-left">
       <div className="order-detail flex-col gap-xs">
-        {userProducts.cart.map((product,index) => {
-          
-          return <ProductTag
-          key={product.product_id}
-          onDelete={deleteProduct}
-          product_id={product.product_id}
-          selectedQuantity={product.quantity}
-          selectedVariant="#02"
-        />
-        }
-          
-        )}
+        {userProducts.cart.map((product, index) => {
+          return (
+            <ProductTag
+              key={product.productId}
+              onDelete={deleteProduct}
+              product_id={product.productId}
+              selectedQuantity={product.quantity}
+              selectedVariant="#02 Rosy"
+            />
+          );
+        })}
       </div>
       <div className="billing-detail flex-col gap-xs">
         <div className="billing-container flex-col gap-sm align-left">
@@ -162,7 +162,7 @@ const Cartpage = () => {
               {userProducts.cart.map((product, index) => {
                 console.log(product);
                 return (
-                  <div 
+                  <div
                     key={product.product_id}
                     className="item flex-row body-sml align-left max-wdth"
                   >
@@ -170,7 +170,9 @@ const Cartpage = () => {
                       {userProducts.product_des[index].product_name}
                     </p>
                     <p className="product-price">
-                      {dongFormatter(userProducts.product_des[index].price * 1000)}
+                      {dongFormatter(
+                        userProducts.product_des[index].price * 1000
+                      )}
                     </p>
                   </div>
                 );
@@ -203,8 +205,12 @@ const Cartpage = () => {
               </div>
               <div className="item-price flex-col gap-xs body-sml align-right">
                 <p className="product-price">{dongFormatter(5000)}</p>
-                <p className="product-price">{dongFormatter(((totalPrice-5)/0.9)*0.2*1000)}(20%)</p>
-                <p className="product-price">{dongFormatter(((totalPrice-5)/0.9)*0.1*1000)}(10%)</p>
+                <p className="product-price">
+                  {dongFormatter(((totalPrice - 5) / 0.9) * 0.2 * 1000)}(20%)
+                </p>
+                <p className="product-price">
+                  {dongFormatter(((totalPrice - 5) / 0.9) * 0.1 * 1000)}(10%)
+                </p>
               </div>
             </div>
           </div>
